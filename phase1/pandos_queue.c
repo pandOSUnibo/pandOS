83aa68cacacc395cda1cf9c21830dd044c9e98ee
lucadonnoh <donnoh99@gmail.com>
2021-01-30 22:40:02 +0100

    prev <-> next

--- phase1/pandos_queue.c
+++ phase1/pandos_queue.c
@@ -46,28 +46,28 @@ void insertProcQ(pcb_t **tp, pcb_t *p) {
         return;
     }
 
-    (*p).p_next = *tp;
-    (*p).p_prev = (**tp).p_prev;
-    (*((*p).p_prev)).p_next = p;
-    (**tp).p_prev = p;
+    (*p).p_prev = *tp;
+    (*p).p_next = (**tp).p_next;
+    (*((*p).p_next)).p_prev = p;
+    (**tp).p_next = p;
     *tp = p;
 }
 
 pcb_t *removeProcQ(pcb_t **tp) {
     if(*tp == NULL) return NULL;
 
-    pcb_t *toRemove = (**tp).p_prev;
+    pcb_t *toRemove = (**tp).p_next;
 
     // Case of a circular list with single element
-    if ((*toRemove).p_prev == toRemove) *tp = NULL;
+    if ((*toRemove).p_next == toRemove) *tp = NULL;
     else {
-        (**tp).p_prev = (*toRemove).p_prev;
-        (*(*toRemove).p_prev).p_next = *tp;
+        (**tp).p_next = (*toRemove).p_next;
+        (*(*toRemove).p_next).p_prev = *tp;
     }
 
     // Resetting pointers of removed process
-    (*toRemove).p_next = NULL;
     (*toRemove).p_prev = NULL;
+    (*toRemove).p_next = NULL;
 
     return toRemove;
 }

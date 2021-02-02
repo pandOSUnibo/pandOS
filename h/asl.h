d36a54eb56f2aab1191b0e7f010f37b446145f0e
elements72 <antonio.lopez.8523@gmail.com>
2021-02-02 12:00:34 +0100

    Minor

--- h/asl.h
+++ h/asl.h
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-#ifndef PANDOS_ASL_H_INCLUDED
-#define PANDOS_ASL_H_INCLUDED
-
-#include "pandos_const.h"
-#include "pandos_types.h"
-
-int insertBlocked(int *semAdd,pcb_t *p);
-=======
 #ifndef ASL_H
 #define ASL_H
 
 #include "pandos_types.h"
 
 int insertBlocked(int *semAdd, pcb_t *p);
->>>>>>> ca6294b0cd0475c779f6465966a1a87caaa95b71
 
 pcb_t* removeBlocked(int *semAdd);
 

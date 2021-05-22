edf257fbea7d0d43bfa37b200e3d99e3af2810ba
Samuele Marro <marrosamuele@gmail.com>
2021-05-22 17:46:55 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ] ; then
-        echo "$0: Insert 'phase1' or 'phase2' or 'phase3 as argument"
+        echo "$0: Insert 'phase1' or 'phase2' or 'phase3' as argument"
         exit 3
 fi
 
-if [ $1 != "phase1" -a $1 != 'phase2'-a $1 != 'phase3' ] ; then
+if [ $1 != "phase1" -a $1 != 'phase2' -a $1 != 'phase3' ] ; then
     echo "$0: Argument not valid, only 'phase1', 'phase2', 'phase3' are allowed"
     exit 1
 else
